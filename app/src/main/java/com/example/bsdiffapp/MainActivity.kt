401b136930f2860bdf614e661fbbd20430059061
duanyufei <duanyufei@bytedance.com>
2021-07-26 23:36:06 +0800

    Add native code

--- app/src/main/java/com/example/bsdiffapp/MainActivity.kt
+++ app/src/main/java/com/example/bsdiffapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.bsdiffapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test()
+    }
+
+    private fun test() {
+        bspatch("src", "patchfile", "dest")
+    }
+
+    external fun bspatch(oldapk: String?, patch: String?, output: String?)
+
+    companion object {
+        init {
+            System.loadLibrary("bspatch")
+        }
     }
 }
